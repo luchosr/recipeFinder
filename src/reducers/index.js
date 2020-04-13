e3b58196814983d3f1bfdfd8e7a42f5fa82a6d7f
Luciano <luchosr@gmail.com>
2020-04-13 13:44:43 -0300

    add actions and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { SET_RECIPES } from "../actions";
+
+function recipes(state = [], action) {
+  switch (action.type) {
+    case "SET_RECIPES":
+      return action.items;
+    default:
+      return state;
+  }
+}
